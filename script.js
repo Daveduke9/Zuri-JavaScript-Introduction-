cdb760e15a24f0854b3a8e17254d81cc663c4be2
IBUKUN OLODUDE <104784071+Daveduke9@users.noreply.github.com>
2022-06-24 08:43:03 +0100

    Name, Height and Country using javascript

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 document.write("Name: OLODUDE IBUKUN DAVID <br> Height: 6ft<br>Country: Nigeria")
-alert("Name: OLODUDE IBUKUN DAVID <br> Height: 6ft<br>Country: Nigeria")
+alert("Name: OLODUDE IBUKUN DAVID \n Height: 6ft \n Country: Nigeria")
